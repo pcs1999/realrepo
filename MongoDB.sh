a99f73212641c2ac1be2247c405f633b0743e317
chandra <chandra@gmail.com>
2023-01-12 14:41:37 +0530

    pcs

--- MongoDB.sh
+++ MongoDB.sh
@@ -2,7 +2,9 @@ set_location=$(pwd)
 cp ${set_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum install mongodb-org -y 
+
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+
 systemctl enable mongod 
 systemctl start mongod 
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 systemctl restart mongod
