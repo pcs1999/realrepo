50bc9ef97a9891b1bf2fb8acfef22b95d8243559
chandra <chandra@gmail.com>
2023-01-12 18:01:10 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -17,4 +17,4 @@ systemctl start catalogue
 cp ${set_location}/files/mongo.repo /etc/yum.repos.d/mongo.repo
 yum install mongodb-org-shell -y
 
-mongo --host 3.82.205.152 </app/schema/catalogue.js
+mongo --host 172.31.83.92 </app/schema/catalogue.js
