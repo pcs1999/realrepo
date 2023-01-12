a3e3bb932d0c9029f0f0e1277bf868628bca426a
chandra <chandra@gmail.com>
2023-01-12 15:06:15 +0530

    pcs

---
+++ catalogue.sh
@@ -0,0 +1,14 @@
+set_location=$(pwd)
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app 
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip 
+cd /app 
+unzip /tmp/catalogue.zip
+cd /app 
+npm install 
+cp ${set_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue 
+systemctl start catalogue
