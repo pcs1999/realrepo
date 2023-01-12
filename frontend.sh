eb6f7b5f3490717472c0355870f32d6b96ea2c2b
chandra <chandra@gmail.com>
2023-01-12 12:11:20 +0530

    pcs

--- frontend.sh
+++ frontend.sh
@@ -1,1 +1,7 @@
-yum install nginx
+yum install nginx -y
+systemctl enable nginx 
+systemctl start nginx
+rm -rf /usr/share/nginx/html/* 
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+vim /etc/nginx/default.d/roboshop.conf 
