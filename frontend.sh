8634982515fb3f028ca862eb8823c30bfc9f65c9
chandra <chandra@gmail.com>
2023-01-13 17:18:02 +0530

    adding learnig folder to learn shell

--- frontend.sh
+++ frontend.sh
@@ -3,27 +3,36 @@ LOG=/tmp/roboshop.log
 
 echo -e "\e[33m installing nginx\e[0m"
 yum install nginx -y &>>${LOG}
+echo $?
 
 echo -e "\e[33m enabling nginx\e[0m"
 systemctl enable nginx &>>${LOG}
+echo $?
 
-prin -e "\e[33m satrting nginx\e[0m"
+echo -e "\e[33m satrting nginx\e[0m"
 systemctl start nginx &>>${LOG}
+echo $?
 
 echo -e "\e[33m removing existing file in html\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${LOG}
+echo $?
 
 echo -e "\e[33m downloading the file from internet\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${LOG}
+echo $?
 
 echo -e "\e[33m changing directory to html\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
+echo $?
 
 echo -e "\e[33m unzipping the files\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
+echo $?
 
 echo -e "\e[33m copying config file to roboshop.conf nginx\e[0m"
 cp ${set_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
+echo $?
 
 echo -e "\e[33m starting nginx\e[0m"
 systemctl restart nginx &>>${LOG}
+echo $?
