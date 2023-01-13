c4362142f4cefe753b8d595fd340584b0fa0b3bb
chandra <chandra@gmail.com>
2023-01-13 18:53:40 +0530

    if ondition adding

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,17 @@
+source function.sh
+
 set_location=$(pwd)
 LOG=/tmp/roboshop.log
 
 echo -e "\e[33m installing nginx\e[0m"
 yum install nginx -y &>>${LOG}
 echo $?
-if [ $? -eq 0 ]
-then
- echo success
-else 
- echo failure
-fi
+condition_check
+
 echo -e "\e[33m enabling nginx\e[0m"
 systemctl enable nginx &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m satrting nginx\e[0m"
 systemctl start nginx &>>${LOG}
@@ -21,23 +20,29 @@ echo $?
 echo -e "\e[33m removing existing file in html\e[0m"
 rm -rf /usr/share/nginx/html/*  &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m downloading the file from internet\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m changing directory to html\e[0m"
 cd /usr/share/nginx/html &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m unzipping the files\e[0m"
 unzip /tmp/frontend.zip &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m copying config file to roboshop.conf nginx\e[0m"
 cp ${set_location}/files/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${LOG}
 echo $?
+condition_check
 
 echo -e "\e[33m starting nginx\e[0m"
 systemctl restart nginx &>>${LOG}
 echo $?
+condition_check
---
+++ learning/function.sh
@@ -0,0 +1,8 @@
+condition_check() {
+    if [ $? -eq 0 ]
+then
+ echo success
+else 
+ echo failure
+fi
+}
