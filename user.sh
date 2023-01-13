b8c3ffcae108a2a2a9973a0fb651858a5a8a8b5b
chandra <chandra@gmail.com>
2023-01-13 14:07:23 +0530

    adding user micro service

--- user.sh
+++ user.sh
@@ -1,7 +1,8 @@
+set_location=$(pwd)
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 yum install nodejs -y
-useradd roboshop
-mkdir /app 
+#useradd roboshop
+mkdir -p /app 
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip 
 cd /app 
 unzip /tmp/user.zip
