99bf899ce43979e6d29da7cb15628a19268cc54c
chandra <chandra@gmail.com>
2023-01-14 14:45:50 +0530

    my sql

--- mysql.sh
+++ mysql.sh
@@ -4,24 +4,27 @@ if [ -z "${mysql_password_}"]; then
   echo "root_mysql_password_is_missing"
   exit 
 fi 
-
+print_head "dnf module"
 dnf module disable mysql -y &>>${LOG}
 condition_check
-
+ 
+print_head "copying files from mysql.repo"
 cp ${set_location}/files/mysql.repo /etc/yum.repos.d/mysql.repo &>>${LOG}
 condition_check
 
+print_head "installing my_sql "
 yum install mysql-community-server -y &>>${LOG} 
 condition_check
 
+print_head "enabling mysql"
 systemctl enable mysqld &>>${LOG}
 condition_check
 
+print_head "start mysql"
 systemctl start mysqld  &>>${LOG}
 condition_check
 
+print_head "set password"
 mysql_secure_installation --set-root-pass ${mysql_password_} &>>${LOG}
 condition_check
 
-mysql -uroot -pRoboShop@1 &>>${LOG}
-condition_check
