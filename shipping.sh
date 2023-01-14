47de0bb21b2f7487bd5ae4ff2d1de062349f575b
chandra <chandra@gmail.com>
2023-01-14 16:46:51 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -4,59 +4,59 @@ set_location=$(pwd)
 LOG=/tmp/roboshop.log
 
 print_head "install maven"
-yum install maven -y &>>${LOG}
+yum install maven -y 
 condition_check
 
 print_head "adding user"
-useradd roboshop &>>${LOG}
+#useradd roboshop 
 condition_check
 
 print_head "adding app direc"
-mkdir /app  &>>${LOG}
+mkdir -p /app  
 condition_check
 
 print_head "curl "
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>${LOG}
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip 
 condition_check
 
 print_head "change directory"
-cd /app  &>>${LOG}
+cd /app  
 condition_check
 
 print_head "unzipping"
-unzip /tmp/shipping.zip &>>${LOG}
+unzip /tmp/shipping.zip 
 condition_check
 
 print_head "change directory"
-cd /app &>>${LOG}
+cd /app 
 condition_check
 
 print_head "clean "
-mvn clean package &>>${LOG}
+mvn clean package 
 condition_check
 
 print_head "target shiiping"
-mv target/shipping-1.0.jar shipping.jar &>>${LOG}
+mv target/shipping-1.0.jar shipping.jar 
 condition_check
 
 print_head "copying"
-cp ${set_location}/files/shipping.service /etc/systemd/system/shipping.service &>>${LOG}
+cp ${set_location}/files/shipping.service /etc/systemd/system/shipping.service 
 condition_check
 
 print_head " reload"
-systemctl daemon-reload &>>${LOG}
+systemctl daemon-reload 
 condition_check
 
 print_head "enable shipping"
-systemctl enable shipping  &>>${LOG}
+systemctl enable shipping  
 condition_check
 
 print_head "start shipping"
-systemctl start shipping &>>${LOG}
+systemctl start shipping 
 condition_check
 
 print_head "restart"
-systemctl restart shipping &>>${LOG}
+systemctl restart shipping 
 condition_check
 
 mysql -h 3.94.9.100 -uroot -pRoboShop@1 < /app/schema/shipping.sql 
